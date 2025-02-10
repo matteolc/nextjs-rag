a9127008ccc5e2c6bd19e4f9884a9855a0f5bff4
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-02-10 17:47:46 +0100

    chore(): fix

--- src/components/palette-page-wrapper.tsx
+++ src/components/palette-page-wrapper.tsx
@@ -3,25 +3,25 @@
 import { ThemePalette, ThemeUtilitiesDisplay } from "@palettebro/theme-toolbar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs";
 
-export function PalettePageWrapper({
-  children,
+export default function PalettePageWrapper({
+	children,
 }: { children?: React.ReactNode }) {
-  return (
-    <Tabs defaultValue="palette" className="space-y-4">
-      <TabsList className="h-8">
-        <TabsTrigger value="palette" className="text-xs px-2 py-1">
-          Palette
-        </TabsTrigger>
-        <TabsTrigger value="utilities" className="text-xs px-2 py-1">
-          Utilities
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="palette">
-        <ThemePalette />
-      </TabsContent>
-      <TabsContent value="utilities">
-        <ThemeUtilitiesDisplay />
-      </TabsContent>
-    </Tabs>
-  );
+	return (
+		<Tabs defaultValue="palette" className="space-y-4">
+			<TabsList className="h-8">
+				<TabsTrigger value="palette" className="text-xs px-2 py-1">
+					Palette
+				</TabsTrigger>
+				<TabsTrigger value="utilities" className="text-xs px-2 py-1">
+					Utilities
+				</TabsTrigger>
+			</TabsList>
+			<TabsContent value="palette">
+				<ThemePalette />
+			</TabsContent>
+			<TabsContent value="utilities">
+				<ThemeUtilitiesDisplay />
+			</TabsContent>
+		</Tabs>
+	);
 }
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,80 +1,88 @@
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
 } from "@/ui/breadcrumb";
 import { Separator } from "@/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/ui/sidebar";
 import { AppSidebar } from "./app-sidebar";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { sentenceCase } from "@/lib/string";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { WorkspaceContext } from "../workspace-context";
+import { workspaces } from "@/app/workspaces";
 
 export const Sidebar = ({ children }: { children: React.ReactNode }) => {
-  const currentWorkspaceName = "Workspace";
-  const pathname = usePathname();
-  const pathSegments = pathname.split("/").filter((segment) => segment !== "");
+	const currentWorkspaceName = useContext(WorkspaceContext);
+	if (!currentWorkspaceName) {
+		return null;
+	}
+	const workspace = workspaces.find(
+		(workspace) => workspace.id === currentWorkspaceName,
+	);
+	const pathname = usePathname();
+	const pathSegments = pathname.split("/").filter((segment) => segment !== "");
 
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <Link
-                    href="/"
-                    className="transition-colors hover:text-foreground"
-                  >
-                    {currentWorkspaceName}
-                  </Link>
-                </BreadcrumbItem>
-                {pathSegments.length > 0 && (
-                  <>
-                    <BreadcrumbSeparator className="hidden md:block" />
-                    {pathSegments.map((segment, index) => {
-                      const href = `/${pathSegments.slice(0, index + 1).join("/")}`;
-                      const isLast = index === pathSegments.length - 1;
-                      return (
-                        <Fragment key={href}>
-                          <BreadcrumbItem className="hidden md:block">
-                            {isLast ? (
-                              <BreadcrumbPage>
-                                {sentenceCase(segment)}
-                              </BreadcrumbPage>
-                            ) : (
-                              <BreadcrumbLink asChild>
-                                <Link
-                                  href={href}
-                                  className="transition-colors hover:text-foreground"
-                                >
-                                  {sentenceCase(segment)}
-                                </Link>
-                              </BreadcrumbLink>
-                            )}
-                          </BreadcrumbItem>
-                          {!isLast && (
-                            <BreadcrumbSeparator className="hidden md:block" />
-                          )}
-                        </Fragment>
-                      );
-                    })}
-                  </>
-                )}
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2">
+					<div className="flex items-center gap-2 px-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator orientation="vertical" className="mr-2 h-4" />
+						<Breadcrumb>
+							<BreadcrumbList>
+								<BreadcrumbItem className="hidden md:block">
+									<Link
+										href="/"
+										className="transition-colors hover:text-foreground"
+									>
+										{workspace?.name}
+									</Link>
+								</BreadcrumbItem>
+								{pathSegments.length > 0 && (
+									<>
+										<BreadcrumbSeparator className="hidden md:block" />
+										{pathSegments.map((segment, index) => {
+											const href = `/${pathSegments.slice(0, index + 1).join("/")}`;
+											const isLast = index === pathSegments.length - 1;
+											return (
+												<Fragment key={href}>
+													<BreadcrumbItem className="hidden md:block">
+														{isLast ? (
+															<BreadcrumbPage>
+																{sentenceCase(segment)}
+															</BreadcrumbPage>
+														) : (
+															<BreadcrumbLink asChild>
+																<Link
+																	href={href}
+																	className="transition-colors hover:text-foreground"
+																>
+																	{sentenceCase(segment)}
+																</Link>
+															</BreadcrumbLink>
+														)}
+													</BreadcrumbItem>
+													{!isLast && (
+														<BreadcrumbSeparator className="hidden md:block" />
+													)}
+												</Fragment>
+											);
+										})}
+									</>
+								)}
+							</BreadcrumbList>
+						</Breadcrumb>
+					</div>
+				</header>
+				<div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+			</SidebarInset>
+		</SidebarProvider>
+	);
 };
