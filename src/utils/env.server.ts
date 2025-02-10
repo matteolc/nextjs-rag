74b8c9eaa9d4157523d4d79c8a6162f4e89615f9
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-02-10 13:04:04 +0100

    feat(): add supabase

---
+++ src/utils/env.server.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const requiredInProduction: z.RefinementEffect<
+	string | undefined
+>["refinement"] = (value, ctx) => {
+	if (process.env.NODE_ENV === "production" && !value) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.custom,
+			message: `Missing required environment variable ${ctx.path.join(".")}`,
+		});
+	}
+};
+
+const requiredInDevelopment: z.RefinementEffect<
+	string | undefined
+>["refinement"] = (value, ctx) => {
+	if (process.env.NODE_ENV === "development" && !value) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.custom,
+			message: `Missing required environment variable ${ctx.path.join(".")}`,
+		});
+	}
+};
+
+const envSchema = z.object({
+	NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+	NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY: z.string(),
+	NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+	BLOB_READ_WRITE_TOKEN: z.string(),
+	OPENAI_API_KEY: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
