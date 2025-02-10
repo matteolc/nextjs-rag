90f9014b13530a5d94cd3495a3fd5b9e6d3a3f76
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-02-10 20:31:11 +0100

    chore(): cleanup

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,20 +1,3 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
-import { InfoIcon } from "lucide-react";
-
-export default async function ProtectedPage() {
-  return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
-      </div>
-    </div>
-  );
+export default function Page() {
+	return <div />;
 }
